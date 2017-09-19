0db633b36c7d8e40034a2dc91935bdf2a8d6ff14
hatelove <jay.hatelove@gmail.com>
2017-09-19 23:21:45 +0800

    extract method: NormalPointCompare(), OneColorCompare() and NoPointCompare()

--- Sibala/Sibala/SibalaComparer.cs
+++ Sibala/Sibala/SibalaComparer.cs
@@ -10,25 +10,40 @@ namespace Sibala
             {
                 if (x.Type == SibalaType.NormalPoint)
                 {
-                    if (x.Points == y.Points)
-                    {
-                        return x.MaxPoint - y.MaxPoint;
-                    }
-
-                    return x.Points - y.Points;
+                    return NormalPointCompare(x, y);
                 }
                 else if (x.Type == SibalaType.OneColor)
                 {
-                    List<int> diceOrder = new List<int> { 2, 3, 5, 6, 4, 1 };
-                    return diceOrder.IndexOf(x.Points) - diceOrder.IndexOf(y.Points);
+                    return OneColorCompare(x, y);
                 }
                 else
                 {
-                    return x.Type - y.Type;
+                    return NoPointCompare(x, y);
                 }
             }
 
             return x.Type - y.Type;
+        }
+
+        private static int NoPointCompare(SibalaGame x, SibalaGame y)
+        {
+            return x.Type - y.Type;
+        }
+
+        private static int OneColorCompare(SibalaGame x, SibalaGame y)
+        {
+            List<int> diceOrder = new List<int> {2, 3, 5, 6, 4, 1};
+            return diceOrder.IndexOf(x.Points) - diceOrder.IndexOf(y.Points);
+        }
+
+        private static int NormalPointCompare(SibalaGame x, SibalaGame y)
+        {
+            if (x.Points == y.Points)
+            {
+                return x.MaxPoint - y.MaxPoint;
+            }
+
+            return x.Points - y.Points;
         }
 
         private static bool IsSameType(SibalaGame x, SibalaGame y)
