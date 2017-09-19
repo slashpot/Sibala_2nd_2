e35ae206548647df6c94d8bb37c7cd0c6e228164
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-09-19 18:34:50 +0800

    Add all SibalaGame tests

--- Sibala/SibalaTests/SibalaGameTests.cs
+++ Sibala/SibalaTests/SibalaGameTests.cs
@@ -9,12 +9,61 @@ namespace Sibala.Tests
     [TestClass()]
     public class SibalaGameTests
     {
-        [TestMethod()]
-        public void SibalaGameTest()
+        private static void Test(List<int> input, string output)
         {
-            var actual = new SibalaGame(new List<int>() { 6, 6, 6, 6 }).Output;
-            
-            Assert.AreEqual("One Color", actual);
+            var actual = new SibalaGame(input).Output;
+
+            Assert.AreEqual(output, actual);
         }
+
+        [TestMethod()]
+        public void SibalaGameTest_OneColor()
+        {
+            Test(new List<int> { 6, 6, 6, 6 }, "One Color");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_DifferentDice_NoPoint()
+        {
+            Test(new List<int> { 1, 2, 3, 4 }, "No Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_3SameNumberDice_NoPoint()
+        {
+            Test(new List<int> { 5, 2, 5, 5 }, "No Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_3121_5Point()
+        {
+            Test(new List<int> { 3, 1, 2, 1 }, "5 Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_4455_10Point()
+        {
+            Test(new List<int> { 4,4,5,5 }, "10 Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_2332_6Point()
+        {
+            Test(new List<int> { 2,3,3,2 }, "6 Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_1123_5Point()
+        {
+            Test(new List<int> { 1,1,2,3 }, "5 Point");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_3312_BG()
+        {
+            Test(new List<int> { 3,3,1,2 }, "BG");
+        }
+        [TestMethod()]
+        public void SibalaGameTest_5566_18la()
+        {
+            Test(new List<int> { 5,5,6,6 }, "18la");
+        }
+
+
+
+
     }
 }
