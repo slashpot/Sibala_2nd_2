e63a047542c7cb456a9a7f663c846aa7f125c3a1
ClaudeLin <linc27595@gmail.com>
2017-09-19 17:55:48 +0800

    rename main sibala

--- Sibala/Sibala/SibalaMain.cs
+++ Sibala/Sibala/SibalaMain.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Sibala
 {
-    class Sibala
+    public class SibalaMain
     {
     }
 }
--- Sibala/SibalaTests/SibalaTest.cs
+++ Sibala/SibalaTests/SibalaTest.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using Sibala;
+using System.Collections.Generic;
 
 namespace SibalaTests
 {
@@ -8,6 +10,18 @@ namespace SibalaTests
         [Test]
         public void SamePoint_Should_Bigger_than_NormalPoint()
         {
+            /*var samePointDice = new List<int> { 1, 1, 1, 1 };
+            var target1 = new SibalaGame(samePointDice);
+            var inputResult1 = target1.GetResult();
+
+            var normalPointDice = new List<int>() { 1, 1, 2, 3 };
+
+            var target2 = new SibalaGame(normalPointDice);
+            var inputResult2 = target2.GetResult();
+
+            var target = new SibalaMain();
+            */
+
         }
     }
 }
