9fab0acdcca3704bebcf30cf6ced43213e19c015
ClaudeLin <linc27595@gmail.com>
2017-09-19 18:35:53 +0800

    refactoring test

--- Sibala/SibalaTests/SibalaTest.cs
+++ Sibala/SibalaTests/SibalaTest.cs
@@ -1,63 +1,58 @@
-﻿using Sibala;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sibala;
 using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace SibalaTests
 {
     [TestClass]
     public class SibalaTest
     {
+        private SibalaGame _inputResult1;
+        private SibalaGame _inputResult2;
+        private SibalaMain _target = new SibalaMain();
+        private int actual;
+        private int expected;
+
         [TestMethod]
         public void SamePoint_Should_Bigger_than_NormalPoint()
         {
-            var inputResult1 = new SibalaGame(new List<int> { 1, 1, 1, 1 });
-            var inputResult2 = new SibalaGame(new List<int> {1, 1, 2, 3});
+            _inputResult1 = new SibalaGame(new List<int> { 1, 1, 1, 1 });
+            _inputResult2 = new SibalaGame(new List<int> { 1, 1, 2, 3 });
+            
+            AssertInput1IsBigger();
+        }
 
-            var target = new SibalaMain();
-            int actual = target.Compare(inputResult1, inputResult2);
-            int expect = 1;
-
-            Assert.AreEqual(expect , actual);
+        private void AssertInput1IsBigger()
+        {
+            actual = _target.Compare(_inputResult1, _inputResult2);
+            Assert.AreEqual(1, actual);
         }
 
         [TestMethod]
         public void NormalPoint_Should_Bigger_than_NoPoint()
         {
+            _inputResult1 = new SibalaGame(new List<int> { 1, 1, 2, 4 });
+            _inputResult2 = new SibalaGame(new List<int> { 1, 5, 2, 3 });
 
-            var inputResult1 = new SibalaGame(new List<int> { 1, 1, 2, 4 });
-            var inputResult2 = new SibalaGame(new List<int> { 1, 5, 2, 3 });
-            
-            var target = new SibalaMain();
-            int actual = target.Compare(inputResult1, inputResult2);
-            int expect = 1;
-
-            Assert.AreEqual(expect, actual);
+            AssertInput1IsBigger();
         }
 
         [TestMethod]
         public void SamePoint_Should_Bigger_than_NoPoint()
         {
-            var inputResult1 = new SibalaGame(new List<int> { 1, 1, 1, 1 });
-            var inputResult2 = new SibalaGame(new List<int> { 1, 5, 2, 3 });
+            _inputResult1 = new SibalaGame(new List<int> { 1, 1, 1, 1 });
+            _inputResult2 = new SibalaGame(new List<int> { 1, 5, 2, 3 });
 
-            var target = new SibalaMain();
-            int actual = target.Compare(inputResult1, inputResult2);
-            int expect = 1;
-
-            Assert.AreEqual(expect, actual);
+            AssertInput1IsBigger();
         }
 
         [TestMethod]
         public void NormalPoint_10_Should_Bigger_than_NormalPoint_6()
         {
-            var inputResult1 = new SibalaGame(new List<int> { 3, 3, 5, 5 });
-            var inputResult2 = new SibalaGame(new List<int> { 1, 1, 3, 3 });
+            _inputResult1 = new SibalaGame(new List<int> { 3, 3, 5, 5 });
+            _inputResult2 = new SibalaGame(new List<int> { 1, 1, 3, 3 });
 
-            var target = new SibalaMain();
-            int actual = target.Compare(inputResult1, inputResult2);
-            int expect = 1;
-
-            Assert.AreEqual(expect, actual);
+            AssertInput1IsBigger();
         }
     }
 }
