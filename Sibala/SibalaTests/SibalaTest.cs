26348d7bff50ac766b5c624b6321da3b1581ab99
ClaudeLin <linc27595@gmail.com>
2017-09-19 18:06:52 +0800

    remove nunit

--- Sibala/SibalaTests/SibalaTest.cs
+++ Sibala/SibalaTests/SibalaTest.cs
@@ -1,29 +1,64 @@
-﻿using NUnit.Framework;
-using Sibala;
-using System.Collections.Generic;
+﻿//using NUnit.Framework;
+//using Sibala;
+//using System.Collections.Generic;
 
-namespace SibalaTests
-{
-    [TestFixture]
-    public class SibalaTest
-    {
-        [Test]
-        public void SamePoint_Should_Bigger_than_NormalPoint()
-        {
-            var samePointDice = new List<int> { 1, 1, 1, 1 };
-            var samePointGame = new SibalaGame(samePointDice);
-            var inputResult1 = samePointGame.GetResult();
+//namespace SibalaTests
+//{
+//    //[TestFixture]
+//    public class SibalaTest
+//    {
+//        //[Test]
+//        public void SamePoint_Should_Bigger_than_NormalPoint()
+//        {
+//            var samePointDice = new List<int> { 1, 1, 1, 1 };
+//            var samePointGame = new SibalaGame(samePointDice);
+//            var inputResult1 = samePointGame.GetResult();
 
-            var normalPointDice = new List<int>() { 1, 1, 2, 3 };
+//            var normalPointDice = new List<int>() { 1, 1, 2, 3 };
+//            var normalGame = new SibalaGame(normalPointDice);
+//            var inputResult2 = normalGame.GetResult();
 
-            var normalGame = new SibalaGame(normalPointDice);
-            var inputResult2 = normalGame.GetResult();
+//            var target = new SibalaMain();
+//            int actual = target.Compare(inputResult1, inputResult2);
+//            int expect = 1;
 
-            var target = new SibalaMain();
-            int actual = target.Compare(inputResult1, inputResult2);
-            int expect = 1;
+//            Assert.AreEqual(expect , actual);
+//        }
 
-            Assert.AreEqual(expect , actual);
-        }
-    }
-}
+//        [Test]
+//        public void NormalPoint_Should_Bigger_than_NoPoint()
+//        {
+//            var normalPoint = new List<int> { 1, 1, 2, 4 };
+//            var normalPointGame = new SibalaGame(normalPoint);
+//            var inputResult1 = normalPointGame.GetResult();
+
+//            var noPoint = new List<int>() { 1, 5, 2, 3 };
+//            var noPointGame = new SibalaGame(noPoint);
+//            var inputResult2 = noPointGame.GetResult();
+
+//            var target = new SibalaMain();
+//            int actual = target.Compare(inputResult1, inputResult2);
+//            int expect = 1;
+
+//            Assert.AreEqual(expect, actual);
+//        }
+
+//        [Test]
+//        public void SamePoint_Should_Bigger_than_NoPoint()
+//        {
+//            var samePoint = new List<int> { 1, 1, 1, 1 };
+//            var samePointGame = new SibalaGame(samePoint);
+//            var inputResult1 = samePointGame.GetResult();
+
+//            var noPoint = new List<int>() { 1, 5, 2, 3 };
+//            var noPointGame = new SibalaGame(noPoint);
+//            var inputResult2 = noPointGame.GetResult();
+
+//            var target = new SibalaMain();
+//            int actual = target.Compare(inputResult1, inputResult2);
+//            int expect = 1;
+
+//            Assert.AreEqual(expect, actual);
+//        }
+//    }
+//}
